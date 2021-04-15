894f70daa8a4030c9d475a5f7ebe813a4c8afb6e
LAPTOP-KJSMBO2J\vicky <mariavictoriabezares@gmail.com>
2021-04-15 18:19:58 -0400

    forma y color de botones CSS

--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
-import { example } from './data.js';
+/*import { example } from './data.js';*/
 import data from './data/lol/lol.js';
-
-
-let pruebaData = data.data; //muestra la data de los campeones en consola
-console.log(pruebaData);
-
-let dataNueva = Object.values(pruebaData);  //.values devuelve las propiedades de los campeones 
-console.log(dataNueva)
-
-dataNueva.forEach(e => console.log(e.name, e.img)); //obtengo nombre e img de los campeones
-
-
-
-
-
-
 
 
 
