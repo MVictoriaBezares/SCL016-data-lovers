1d83bb5fa30dfaa97d037fd9cf17a57443bb28db
LAPTOP-KJSMBO2J\vicky <mariavictoriabezares@gmail.com>
2021-05-03 11:12:56 -0400

    ultimo commit

--- src/main.js
+++ src/main.js
@@ -1,45 +1,140 @@
-/*import { example } from './data.js';*/
-import data from './data/lol/lol.js';
+////import data from './data/lol/lol.json';////
+import champ from './data/lol/lol.js';
 
-/*innerHTML: propiedad DOM que establece o devuelve el contenido HTML interno) de un elemento.*/
-/*createElement: método crea un nodo de elemento con el nombre especificado. Después de crear el elemento, utilice el elemento.appendChild() o elemento.insertBefore() método para insertarlo en el documento.
-/*appendChild: método DOM anexa un nodo como el último elemento secundario de un nodo.Agrega nvo elemento a una lista*/
-/*template: etiqueta se utiliza como contenedor para contener algo de contenido HTML oculto cuando se carga web. El contenido interior se puede representar más adelante con un JS. Puede usar la etiqueta si tiene algún código HTML que desea usar una y otra vez, pero no hasta que lo solicite. Para ello sin la etiqueta, debe crear el código HTML con JavaScript para evitar que el explorador represente el código.<template><template>*/
-/*forEach metodo que llama a una f(x) una vez para cada elemento de una matriz en orden*/
+////Muestra la data de los campeones en consola////
+let totalData = champ.data;                       
+let dataChampions = Object.values(totalData);  ///para guardar los objetos y sus valores de la data, Object: son contenedores para valores
+                                                   
+const modal= document.createElement('div');   
+modal.setAttribute('id', 'champ-modal');   ///"champ-modal" uno lo crea con setAtribute, se crea de etiquetas///
 
-let pruebaData = data.data; //muestra la data de los campeones en consola*/
+/////Muestra todos los campeones inicialmente/////
+showChampions(dataChampions);                   /// se pueden crear f(x) en cualquier parte y se definen despues///
 
-let dataNueva = Object.values(pruebaData);  /*.values devuelve las propiedades de los campeones*/ 
-/*console.log(dataNueva)*/
+///Asignando funciones a botones(filtro por tipo)///
+let buttonAssassin = document.getElementById('assassin');
+buttonAssassin.addEventListener('click', () => {
+  cleanScreenOfChampions();                      /// porque es funcion
+  leakedChampions('Assassin');                    // se le pide que filtre los campeones asesinos
+});
 
-dataNueva.forEach(e => console.log(e.name, e.img)); //obtengo nombre e img de los campeones
+let buttonFighter = document.getElementById('fighter');  ///Luchadores
+buttonFighter.addEventListener('click', () => {
+  cleanScreenOfChampions();
+  leakedChampions('Fighter');
+});
 
-let myBttn= document.addEventListener("click" (e.name, e.img));
+let buttonMage = document.getElementById('mage');
+buttonMage.addEventListener('click', () => {
+  cleanScreenOfChampions();
+  leakedChampions('Mage');
+});
 
-function myBttn() {
-  var btn = document.createElement("button");  // para crear boton
-  btn.innerHTML = "Akali";
-  document.body.appendChild(btn);
+let buttonMarksman = document.getElementById('marksman');  ///Tiradores
+buttonMarksman.addEventListener('click', () => {
+  cleanScreenOfChampions();
+  leakedChampions('Marksman');
+});
+
+let buttonSupport = document.getElementById('support');   ///Soportes
+buttonSupport.addEventListener('click', () => {
+  cleanScreenOfChampions();
+  leakedChampions('Support');
+});
+
+let buttonTank = document.getElementById('tank');    ///tanques
+buttonTank.addEventListener('click', () => {
+  cleanScreenOfChampions();
+  leakedChampions('Tank');
+});
+
+////Boton Reset/////
+let buttonReset = document.getElementById('reset');
+buttonReset.addEventListener('click', () => {
+  viewDataNew();                               // muestra todos los campeones de nuevo una vez q esta filtrado
+});
+
+///funciones limpieza///
+function cleanScreenOfChampions() {
+  document.getElementById('root').innerHTML = '';  ///innerHTML le agrego naada; root es id de div///
 }
 
+///Funcion que muestra toda la data denuevo///
+function viewDataNew() {
+  cleanScreenOfChampions();
+  showChampions(dataChampions);
+}
 
-
-
-
-
-function campeon () {    /*el valor de esta variable no se lee*/
-  let text= " ";
-  let e;
-  
-  for (let e in dataNueva) {
-    let nombreImg = dataNueva.forEach;
-    /*let botonReset= document.getElementById("root"); botonReset.innerHTML = nombreImg;*/
-  
+////Función que muestra los Campeones////   
+function showChampions(champions) {
+  for (const obj of champions) {               ///recorre los valores de la data. obj: es la data misma///
+    let pName = document.createElement('p');
+    let divIcons = document.createElement('div');
+    divIcons.setAttribute('class', 'champ-container');  //class es lo que quieres crear, nombre de class///
     
-    console.log(campeon);
+    pName.textContent = obj.name;
+
+    //crear contenedor para nombre otro para img (divIcons) es una capsula dentro de otra//  
+    let divRoot = document.getElementById('root');
+    divIcons.appendChild(pName);                
+
+    let img = document.createElement('img');
+    img.setAttribute('class', 'img-container'); //agregar atributos del click. Filtrar para mostrar info///
+
+    img.src = obj.img;
+
+    divIcons.appendChild(img);
+    divRoot.appendChild(divIcons);  ///hasta aqui tenemos mostrado img y nombres
+
+    const closeModal= () => {
+      divRoot.removeChild(modal);
+    };
+
+    divIcons.addEventListener("click", () => {
+      let champInfo = `
+                    <div class="div-container">
+                      <button value="Cerrar divIcons" id="close-modal-button" />X</button>
+                      <p class="title-modal"> ${obj.title}</p>
+                      <img class="img-modal" src="${obj.splash}"/>
+                      <p class="blurb-modal">${obj.blurb}</p> 
+                    </div> `;
+
+      modal.innerHTML = champInfo;
+      divRoot.appendChild(modal)
+
+      const button = document.getElementById("close-modal-button") ///se creo en 108 para cerrar pantalla///
+      button.onclick = closeModal
+
+    });
   }
 }
 
+////Función que filtra los Campeones y los muestra en pantalla////
+function leakedChampions(tag) {
+  let championsToShow = dataChampions.filter(lolData =>
+    lolData.tags.includes(tag));    //tag es lo q quiero q filtre, hasta aqui se filtra
+    showChampions(championsToShow);
+}
 
+////ordenarrrrr
+const dataAZ = dataChampions.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;                      //retorma p+ a, b, c
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
+});
 
-//console.log(example, data);
+  const orderAZ = document.getElementById("selectOrder");  //id de bt ordenar
+  orderAZ.addEventListener("change", () => {              /// change evento que cambie, 1° limpia luego ordene A-Z                 
+    cleanScreenOfChampions();
+    showChampions(dataAZ);
+  });
+
+  const orderZA = document.getElementById("selectOrder");
+  orderZA.addEventListener("change", () => {
+    cleanScreenOfChampions();
+    showChampions(dataAZ.reverse()); ///inv si es metodo
+  });
